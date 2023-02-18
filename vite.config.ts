2abef567ae7856df48b78e67c3a4b2a07c6b4a20
hasoseso-Gunnar <thesuperhyperhelix@gmail.com>
2023-02-18 16:36:02 +0900

    changes on vite.config.ts again

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: (process.env.NODE_ENV === 'poduction')
-    ? '/gunnar-hp/' : '/',
  build: {
    outDir: 'docs'
  },
